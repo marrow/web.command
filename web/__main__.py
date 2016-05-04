3deb58ddb3c164e454911996542c6200a62242e9
Alice Bevan–McGregor <alice@gothcandy.com>
2016-05-04 04:24:33 -0400

    Color! Metadata! And the ugliest code I have written in years.

--- web/__main__.py
+++ web/__main__.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 
 import sys
 import select
-import codecs
+import logging
 import pkg_resources
 
 from marrow.script import describe
@@ -18,69 +18,46 @@ __all__ = ['ScriptCore']
 
 
 class ScriptCore(object):
-    """Extensible command line script dispatcher for the WebCore web framework."""
-    
-    _cmd_script = dict(
-            title = "web",
-            version = release.version,
-            copyright = "Copyright 2012 Alice Bevan-McGregor and contributors"
-        )
-    
-    @describe(
-            verbose="Increase logging level to DEBUG.",
-            quiet="Reduce logging level to WARN.",
-            config="The configuration file to use.",
-            log="Where to send logging output; can use STDOUT and STDERR or the specifically named file.",
-            log_level="The default logging level.\nSee marrow.logging documentation for valid values."
-        )
-    def __init__(self, verbose=False, quiet=False, config="local.yaml", log="STDERR", log_level="info"):
-        self.verbose = verbose
-        self.quiet = quiet
-        self._config = config
-        self._config_cache = None
-        self.log = log
-        self.log_level = log_level
-        import logging
-        logging.basicConfig(level=logging.DEBUG)
-    
-    @property
-    def config(self):
-        if not self._config_cache:
-            if not sys.stdin.isatty() and select.select([sys.stdin],[],[],0.0)[0]:
-                # We have configuration coming in over STDIN.
-                pass
-				#self._config_cache = Configuration.load(sys.stdin)
-            else:
-                try:
-                    pass
-					#self._config_cache = Configuration.load(codecs.open(self._config, 'r', 'utf8'))
-                except FileNotFoundError:
-                    raise ExitException(
-                        "Configuration file not found: " + self._config +
-                        "\n\nSpecify one using the --config (-c) global command-line argument."
-                    )
-        
-        return self._config_cache
-    
-    # Load plugins.
-    locals().update({
-            i.name: i.load()
-            for i in pkg_resources.iter_entry_points('web.command')
-        })
-    
-    def __getattr__(self, name):
-        if not name.endswith('.yaml'):
-            raise AttributeError("'ScriptCore' object has no attribute '"+name+"'")
-        
-        self._config = name
-        
-        return getattr(self, self.config['exec'].command)
+	"""Extensible command line script dispatcher for the WebCore web framework."""
+	
+	_cmd_script = dict(
+			title = "web",
+			version = release.version,
+			copyright = "Copyright 2012 Alice Bevan-McGregor and contributors"
+		)
+	
+	@describe(
+			verbose="Increase the default logging level to DEBUG.",
+			quiet="Reduce the default logging level to WARN.",
+			config="Specify a configuration file to use.",
+			log="Where to send logging output; can use STDOUT and STDERR or the specifically named file.",
+		)
+	def __init__(self, verbose=False, quiet=False, config="local.yaml", log="STDERR"):
+		self.verbose = verbose
+		self.quiet = quiet
+		self._config = config
+		self.log = log
+		
+		levels = [logging.WARN, logging.INFO, logging.DEBUG]
+		logging.basicConfig(level=levels[int(verbose)-int(quiet)+1])
+	
+	# Load plugins.
+	locals().update({
+			i.name: i.load()
+			for i in pkg_resources.iter_entry_points('web.command')
+		})
+	
+	def __getattr__(self, name):
+		if not name.endswith('.yaml'):
+			raise AttributeError("'ScriptCore' object has no attribute '"+name+"'")
+		
+		self._config = name
 
 
 def main():
-    from marrow.script import execute
-    execute(ScriptCore)
+	from marrow.script import execute
+	sys.exit(execute(ScriptCore))
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- web/command/versions.py
+++ web/command/versions.py
@@ -8,90 +8,131 @@ from marrow.script import Parser
 from marrow.script.util import wrap
 
 
+E = "\033["
+NO_COLOR = {'b': '', 'item': '', 'n': '', 'plug': '', 'pkg': '', 'ver': '', 'label': '', 'extra': '', 'extraf': ''}
+COLOR = {'b': E+'1m', 'item': E+'1;32m', 'n': E+'0m', 'plug': E+'37;1m', 'pkg': E+'1;32m', 'ver': E+'33m', 'label': E+'32;2m', 'extra': E+'36m', 'extraf': E+'1;31m'}
+C = COLOR
+
+MD = ['Summary', 'Home-page', 'Author', 'Author-email', 'License']
+
 def explore(namespace, short):
-    for i in pkg_resources.iter_entry_points(namespace):
-        if not short:
-            print(" * \"{0}\" provided by {1} {2} from:".format(i.name, i.dist.project_name, i.dist.version))
-            print("   ", "   ".join(wrap(i.dist.location, Parser.width() - 3).split("\n")), sep='', end="\n\n")
-        else:
-            print("{0} ({1} {2})".format(i.name, i.dist.project_name, i.dist.version))
+	for i in pkg_resources.iter_entry_points(namespace):
+		if not short:
+			metadata = dict(j.partition(': ')[::2] for j in i.dist._get_metadata(i.dist.PKG_INFO) if j.partition(':')[0] in MD)
+			line = C['n'] + C['plug'] + i.name + C['n'] + " provided by " + C['pkg'] + \
+					i.dist.project_name + " " + C['ver'] + i.dist.version + C['n']
+			llen = len(line) - (3 + 3*len(C['n']) + len(C['item']) + len(C['plug']) + len(C['pkg']) + len(C['ver']))
+			fail = []
+			if i.extras:
+				extras = []
+				for extra in sorted(i.extras):
+					if extra in i.dist._dep_map:  # It's an "extras_require".
+						for dep in i.dist._dep_map[extra]:
+							try:
+								pkg_resources.require(str(dep))
+							except pkg_resources.DistributionNotFound:
+								fail.append(C['extraf'] + str(dep) + C['n'])
+					if fail:
+						extras.append(C['extraf'] + extra + C['n'] + " (missing: " + ", ".join(fail) + ")")
+					else:
+						extras.append(C['extra'] + extra + C['n'])
+				print(" ", C['extraf'] if fail else C['item'], "* ", C['n'], line, C['extra'], C['b'], " + ", C['n'], ("\n "+C['extra']+C['b']+"+ "+C['n']).join(wrap(" ".join(extras), Parser.width() - llen).split("\n")), C['n'], sep='')
+			else:
+				print(" ", C['item'], "* ", C['n'], line, sep='')
+			def md(label, line):
+				labl = 12 - len(label)
+				print("   ", C['label'], label, ":", labl * " ", C['n'], "\n   ".join(wrap(line, Parser.width() - (3+len(label)+2)).split("\n")), sep='')
+			if 'Summary' in metadata: md('Description', metadata['Summary'])
+			md("Location", i.dist.location)
+			if 'Author' in metadata: md('Author', metadata['Author'] + ((' <' + metadata['Author-email'] + '>') if metadata.get('Author-email') else ''))
+			if 'Home-page' in metadata: md('Homepage', metadata['Home-page'])
+			if metadata.get('License'): md('License', metadata['License'])
+		else:
+			print("{0} ({1} {2})".format(i.name, i.dist.project_name, i.dist.version))
+		print()
 
 
 def dependencies(package, short):
-    seen = dict()
-    for i in pkg_resources.require(package):
-        if i in seen: continue
-        seen[i] = None
-        
-        if not short:
-            print(" * {0} {1} from:".format(i.project_name, i.version))
-            print("   ", "   ".join(wrap(i.location, Parser.width() - 3).split("\n")), sep='', end="\n\n")
-        else:
-            print("{0} {1}".format(i.project_name, i.version))
+	seen = dict()
+	for i in pkg_resources.require(package):
+		if i in seen: continue
+		seen[i] = None
+		
+		if not short:
+			print(" * {0} {1} from:".format(i.project_name, i.version))
+			print("   ", "	 ".join(wrap(i.location, Parser.width() - 3).split("\n")), sep='', end="\n\n")
+		else:
+			print("{0} {1}".format(i.project_name, i.version))
 
 
 def all_packages(short):
-    seen = dict()
-    for i in pkg_resources.working_set:
-        if i in seen: continue
-        seen[i] = None
-        
-        if not short:
-            print(" * {0} {1} from:".format(i.project_name, i.version))
-            print("   ", "   ".join(wrap(i.location, Parser.width() - 3).split("\n")), sep='', end="\n\n")
-        else:
-            print("{0} {1}".format(i.project_name, i.version))
+	seen = dict()
+	for i in pkg_resources.working_set:
+		if i in seen: continue
+		seen[i] = None
+		
+		if not short:
+			print(" * {0} {1} from:".format(i.project_name, i.version))
+			print("   ", "	 ".join(wrap(i.location, Parser.width() - 3).split("\n")), sep='', end="\n\n")
+		else:
+			print("{0} {1}".format(i.project_name, i.version))
 
 
 def all_namespaces():
-    counts = dict()
-    
-    for i in pkg_resources.working_set:
-        plugins = pkg_resources.get_entry_map(i)
-        for k in plugins:
-            counts.setdefault(k, 0)
-            counts[k] += len(plugins[k])
-    
-    for k in sorted(counts):
-        print(" * {0} ({1} plugins)".format(k, counts[k]))
+	counts = dict()
+	
+	for i in pkg_resources.working_set:
+		plugins = pkg_resources.get_entry_map(i)
+		for k in plugins:
+			counts.setdefault(k, 0)
+			counts[k] += len(plugins[k])
+	
+	for k in sorted(counts):
+		print(" * {0} ({1} plugins)".format(k, counts[k]))
 
 
-def versions(self, all=False, package='WebCore', namespace=None, namespaces=False, short=False):
-    """Display versions of installed packages.
-    
-    Use the namespace argument to enumerate all plugins for the given namespace.  Examples include: console_scripts, web.command, web.db.engines, web.dispatch, marrow.templating, ...
-    
-    Example calls:
-    
-    web versions --all
-    # dump all installed packages
-    
-    web versions --package mako
-    # dump requirements for specific package
-    
-    web versions --namespaces
-    # show available plugin namespaces
-    
-    web versions --namespace marrow.templating
-    # show all plugins for this namespace
-    
-    web versions --short
-    # one line per requirement, no paths or formatting
-    """
-    
-    # TODO: Determine if versions are latest, and if safe upgrading is possible.
-    
-    if namespace:
-        print("Plugins within the \"{0}\" namespace:\n".format(namespace))
-        return explore(namespace, short)
-    
-    if all:
-        print("All installed Python packages:\n")
-        return all_packages(short)
-    
-    if namespaces:
-        print("Plugin namespaces:\n")
-        return all_namespaces()
-    
-    print(package, "and dependencies:\n")
-    return dependencies(package, short)
+def versions(self, all=False, package='WebCore', namespace=None, namespaces=False, short=False, color=True):
+	"""Display versions of installed packages.
+	
+	Use the namespace argument to enumerate all plugins for the given namespace.  Examples include: console_scripts, web.command, web.db.engines, web.dispatch, marrow.templating, ...
+	
+	Example calls:
+	
+	web versions --all
+	# dump all installed packages
+	
+	web versions --package mako
+	# dump requirements for specific package
+	
+	web versions --namespaces
+	# show available plugin namespaces
+	
+	web versions --namespace marrow.templating
+	# show all plugins for this namespace
+	
+	web versions --short
+	# one line per requirement, no paths or formatting
+	"""
+	
+	global C
+	
+	# TODO: Determine if versions are latest, and if safe upgrading is possible.
+	if color:
+		C = COLOR
+	else:
+		C = NO_COLOR
+	
+	if namespace:
+		print("Plugins within the \"{0}\" namespace:\n".format(namespace))
+		return explore(namespace, short)
+	
+	if all:
+		print("All installed Python packages:\n")
+		return all_packages(short)
+	
+	if namespaces:
+		print("Plugin namespaces:\n")
+		return all_namespaces()
+	
+	print(package, "and dependencies:\n")
+	return dependencies(package, short)
